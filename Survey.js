61de7e138e03608fc3ebbbdac688edb613f8b4a0
Mounica Chirva <mounicachirva@gmail.com>
2016-10-09 16:37:03 -0400

    Trial#1 Allowed control over origin

--- Survey.js
+++ Survey.js
@@ -185,22 +185,24 @@ router.route('/getResult/:userId')
         })
     });
 
-  app.use('/api', router);
 // Add headers
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Origin',"*");
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,accept');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     // Pass to next layer of middleware
     next();
 });
+
+app.use('/api', router);
+
   app.listen(8080, function() {
     console.log("✔ Express server listening on port %d in %s mode", 8080, app.get('env'));
   });
