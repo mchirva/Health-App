e5e4c342c6a0530f7f047a51007463e66e130347
Mounica Chirva <mounicachirva@gmail.com>
2016-10-13 21:23:16 -0400

    Allowing headers

--- Survey.js
+++ Survey.js
@@ -223,7 +223,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Accept, Content-Type, Access-Control-Request-Headers");
 
     // Pass to next layer of middleware
     next();
